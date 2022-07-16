86bbe2d04e479ece0f4dd7df37292b92484b2737
MARLAN <cpenarsky@shaw.ca>
2022-07-16 16:36:21 -0500

    rearrange code

--- weather-update/src/main/java/com/marlan/weatherupdate/controller/DCSRealWeather.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/controller/DCSRealWeather.java
@@ -18,11 +18,12 @@ public class DCSRealWeather {
     public static void main(String[] args) throws Exception {
         final Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
         final String DIR = getProperty("user.dir") + "\\";
+        final String MISSION_FILE_NAME = "mission";
+        
         AVWXClient avwxClient = new AVWXClient();
         MizHandlerService mizHandlerService = new MizHandlerService();
         FileHandlerService fileHandlerService = new FileHandlerService();
         MissionHandlerService missionHandlerService = new MissionHandlerService();
-        final String MISSION_FILE_NAME = "mission";
 
         String dataContent = fileHandlerService.readFile(DIR, "Data.txt");
         WeatherUpdateData weatherUpdateData = gson.fromJson(dataContent, WeatherUpdateData.class);
