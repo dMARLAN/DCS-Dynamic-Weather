162a0e7fd27a632c54f91accf78785e92d682883
MARLAN <cpenarsky@shaw.ca>
2022-07-16 17:03:29 -0500

    create buildCloudsPreset method and refactor

--- weather-update/src/main/java/com/marlan/weatherupdate/service/MissionHandlerService.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/service/MissionHandlerService.java
@@ -13,75 +13,57 @@ import static java.lang.System.out;
 @NoArgsConstructor
 public class MissionHandlerService {
     public String editMission(String mission, AVWXWeather weatherAVWX) throws NoSuchAlgorithmException {
+        ZuluLocalConverter zuluLocalConverter = new ZuluLocalConverter();
         double qnhInMg = weatherAVWX.getAltimeter().getValue() * 25.4;
         double tempC = weatherAVWX.getTemperature().getValue();
         double windSpeed = Math.min(weatherAVWX.getWindSpeed().getValue() / 1.944, 15);
         double windDir = weatherAVWX.getWindDirection().getValue();
-        ZuluLocalConverter zuluLocalConverter = new ZuluLocalConverter();
         String metar = weatherAVWX.getSanitized();
         int timeInSeconds = ((Integer.parseInt(weatherAVWX.getTime().getRepr().substring(2, 4)) + zuluLocalConverter.zuluToLocalConversion(weatherAVWX.getStation())) % 24) * 3600;
+        String cloudsPreset = buildCloudsPreset(selectCloudsPresetSuffix(metar));
 
-        String cloudsPreset;
-        int cloudsPresetSuffix = selectCloudsPresetSuffix(metar);
-        if (cloudsPresetSuffix == 0) {
-            cloudsPreset = "nil";
-        } else {
-            if (cloudsPresetSuffix > 27) {
-                cloudsPreset = "RainyPreset" + cloudsPresetSuffix % 27; // Converts Presets28-30 to RainyPreset1-3
-            } else {
-                cloudsPreset = "Preset" + cloudsPresetSuffix;
-            }
-        }
-
-        String regexCloudsPreset;
-        String replaceCloudsPreset;
         if (!mission.contains("[\"preset\"]")) {
-            regexCloudsPreset = "(\\[\"iprecptns\"].*)\n";
-            replaceCloudsPreset = ("[\"iprecptns\"] = 0,\n            [\"preset\"] = \"$cloudsPreset\",\n").replace("$cloudsPreset", cloudsPreset);
+            mission = mission.replaceAll("(\\[\"iprecptns\"].*)\n", "[\"iprecptns\"] = 0,\n            [\"preset\"] = \"$cloudsPreset\",\n").replace("$cloudsPreset", cloudsPreset);
         } else {
-            regexCloudsPreset = "(\\[\"preset\"].*)\n";
-            replaceCloudsPreset = "[\"preset\"] = \"$cloudsPreset\",\n".replace("$cloudsPreset", cloudsPreset);
+            mission = mission.replaceAll("(\\[\"preset\"].*)\n", "[\"preset\"] = \"$cloudsPreset\",\n".replace("$cloudsPreset", cloudsPreset));
         }
-        mission = mission.replaceAll(regexCloudsPreset, replaceCloudsPreset);
         out.println("Clouds Preset set to: " + cloudsPreset);
 
-        // TODO: Match Ground Speed only?
-        String regexWindSpeed = "(\\[\"speed\"].*)\n";
-        String replaceWindSpeed = "[\"speed\"] = $windSpeed,\n".replace("$windSpeed", Double.toString(windSpeed));
-        mission = mission.replaceAll(regexWindSpeed, replaceWindSpeed);
+        // TODO: Match Ground only?
+        mission = mission.replaceAll("(\\[\"speed\"].*)\n", "[\"speed\"] = $windSpeed,\n".replace("$windSpeed", Double.toString(windSpeed)));
         out.println("Wind Speed set to: " + windSpeed + " m/s");
 
-        String regexWindDir = "(\\[\"dir\"].*)\n";
-        String replaceWindDir = "[\"dir\"] = $windDir,\n".replace("$windDir", Double.toString(windDir));
-        mission = mission.replaceAll(regexWindDir, replaceWindDir);
+        mission = mission.replaceAll("(\\[\"dir\"].*)\n", "[\"dir\"] = $windDir,\n".replace("$windDir", Double.toString(windDir)));
         out.println("Wind Direction set to: " + windDir);
 
-        String regexStartTime = "(\\[\"start_time\"].*)\n";
-        String replaceStartTime = "[\"start_time\"] = $startTime,\n".replace("$startTime", Integer.toString(timeInSeconds));
-        mission = mission.replaceAll(regexStartTime, replaceStartTime);
+        mission = mission.replaceAll("(\\[\"start_time\"].*)\n", "[\"start_time\"] = $startTime,\n".replace("$startTime", Integer.toString(timeInSeconds)));
         out.println("Start Time set to: " + timeInSeconds + "s");
 
-        String regexDay = "(\\[\"Day\"].*)\n";
-        String replaceDay = "[\"Day\"] = $day,\n".replace("$day", Integer.toString(Integer.parseInt(weatherAVWX.getTime().getRepr().substring(0, 2))));
-        mission = mission.replaceAll(regexDay, replaceDay);
+        mission = mission.replaceAll("(\\[\"Day\"].*)\n", "[\"Day\"] = $day,\n".replace("$day", Integer.toString(Integer.parseInt(weatherAVWX.getTime().getRepr().substring(0, 2)))));
         out.println("Day set to: " + Integer.parseInt(weatherAVWX.getTime().getRepr().substring(0, 2)));
 
-        String regexMonth = "(\\[\"Month\"].*)\n";
-        String replaceMonth = "[\"Month\"] = $month,\n".replace("$month", Integer.toString(Integer.parseInt(weatherAVWX.getTime().getDt().substring(5, 7))));
-        mission = mission.replaceAll(regexMonth, replaceMonth);
+        mission = mission.replaceAll("(\\[\"Month\"].*)\n", "[\"Month\"] = $month,\n".replace("$month", Integer.toString(Integer.parseInt(weatherAVWX.getTime().getDt().substring(5, 7)))));
         out.println("Month set to: " + Integer.parseInt(weatherAVWX.getTime().getDt().substring(5, 7)));
 
-        String regexTempC = "(\\[\"temperature\"].*)\n";
-        String replaceTempC = "[\"temperature\"] = $tempC,\n".replace("$tempC", Double.toString(tempC));
-        mission = mission.replaceAll(regexTempC, replaceTempC);
+        mission = mission.replaceAll("(\\[\"temperature\"].*)\n", "[\"temperature\"] = $tempC,\n".replace("$tempC", Double.toString(tempC)));
         out.println("Temperature set to: " + tempC + "C");
 
-        String regexQNH = "(\\[\"qnh\"].*)\n";
-        String replaceQNH = "[\"qnh\"] = $qnh,\n".replace("$qnh", Double.toString(qnhInMg));
-        mission = mission.replaceAll(regexQNH, replaceQNH);
+        mission = mission.replaceAll("(\\[\"qnh\"].*)\n", "[\"qnh\"] = $qnh,\n".replace("$qnh", Double.toString(qnhInMg)));
         out.println("QNH set to: " + qnhInMg + "inHg");
 
         return mission;
+    }
+
+    private String buildCloudsPreset(int cloudsPresetSuffix){
+        if (cloudsPresetSuffix == 0) {
+            return "nil";
+        } else {
+            if (cloudsPresetSuffix > 27) {
+                return "RainyPreset" + cloudsPresetSuffix % 27; // Converts Presets28-30 to RainyPreset1-3
+            } else {
+                return "Preset" + cloudsPresetSuffix;
+            }
+        }
     }
 
     private int selectCloudsPresetSuffix(String metar) throws NoSuchAlgorithmException {
