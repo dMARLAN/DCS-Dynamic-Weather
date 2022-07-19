7df8ee0322a4953c7b4508f164fd197105a0b052
MARLAN <cpenarsky@shaw.ca>
2022-07-18 19:04:48 -0500

    add handling for various DCS airbases

--- weather-update/src/main/java/com/marlan/weatherupdate/utilities/ZuluLocalConverter.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/ZuluLocalConverter.java
@@ -7,7 +7,12 @@ import static java.lang.System.out;
 @NoArgsConstructor
 public class ZuluLocalConverter {
     public int zuluToLocalConversion(String icao) {
-        if (icao.equals("KLSV")) return -6; // TODO: Expand on this!
+        if (icao.equals("KLSV")) return -7; // Nellis AFB
+        if (icao.equals("OMAM")) return 4; // Al Dhafra AFB
+        if (icao.equals("PGUA")) return -2; // Andersen AFB
+        if (icao.equals("UGSB")) return 4; // Batumi AFB
+        if (icao.equals("LTAG")) return 3; // Incirlik AFB
+        if (icao.equals("EGYP")) return 3; // Mount Pleasant AFB
         out.println("ZuluLocalConverter: ICAO Unknown, time conversion set to 0");
         return 0;
     }
