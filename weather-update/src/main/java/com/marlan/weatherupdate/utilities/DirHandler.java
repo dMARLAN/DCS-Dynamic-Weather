b9d2a15cef7b0b8a52820e8fdab572c75c305916
MARLAN <cpenarsky@shaw.ca>
2022-07-20 13:49:57 -0500

    set to utility class with lombok

--- weather-update/src/main/java/com/marlan/weatherupdate/utilities/DirHandler.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/DirHandler.java
@@ -1,9 +1,12 @@
-package com.marlan.weatherupdate.service;
+package com.marlan.weatherupdate.utilities;
+
+import lombok.experimental.UtilityClass;
 
 import static java.lang.System.getProperty;
 
+@UtilityClass
 public class DirHandler {
-    public String getWorkingDir(String[] args){
+    public static String getWorkingDir(String[] args){
         if (args.length != 0) {
             System.setProperty("user.dir", args[0]);
         }
