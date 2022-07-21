ffc8fb07e4ba7291ce5052c59ba12c0da386b9be
MARLAN <cpenarsky@shaw.ca>
2022-07-20 21:11:58 -0500

    add some error handling

--- weather-update/src/main/java/com/marlan/weatherupdate/service/FileHandler.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/service/FileHandler.java
@@ -4,29 +4,37 @@ import lombok.NoArgsConstructor;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @NoArgsConstructor
 public class FileHandler {
 
-    public String readFile(String dir, String fileName) throws IOException {
-        Path dataFilePath = Path.of(dir + fileName);
-        return Files.readString(dataFilePath);
+    public String readFile(String dir, String fileName) {
+        try {
+            Path filePath = Path.of(dir + fileName);
+            return Files.readString(filePath);
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
+        return null;
     }
 
     public void overwriteFile(String dir, String fileName, String newContent) {
-        File missionFile = new File(dir + fileName);
-        try (FileWriter fileWriter = new FileWriter(missionFile, false)) {
+        File file = new File(dir + fileName);
+        try (FileWriter fileWriter = new FileWriter(file, false)) {
             fileWriter.write(newContent);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void deleteFile(String dir, String fileName){
-        File file = new File(dir + fileName);
-        file.deleteOnExit();
+    public void deleteFile(String dir, String fileName) {
+        try {
+            Path filePath = Path.of(dir + fileName);
+            Files.delete(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
