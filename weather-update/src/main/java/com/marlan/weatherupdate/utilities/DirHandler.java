c46f8efbe4342813a6f3584570ae9405406df7d2
MARLAN <cpenarsky@shaw.ca>
2022-07-20 21:12:49 -0500

    improve log message

--- weather-update/src/main/java/com/marlan/weatherupdate/utilities/DirHandler.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/DirHandler.java
@@ -3,13 +3,15 @@ package com.marlan.weatherupdate.utilities;
 import lombok.experimental.UtilityClass;
 
 import static java.lang.System.getProperty;
+import static java.lang.System.out;
 
 @UtilityClass
 public class DirHandler {
-    public static String getWorkingDir(String[] args){
+    public static String getWorkingDir(String[] args) {
         if (args.length != 0) {
             System.setProperty("user.dir", args[0]);
         }
+        out.println("INFO: Working Directory: " + getProperty("user.dir") + "\\");
         return getProperty("user.dir") + "\\";
     }
 }
