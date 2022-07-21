358e195f130b7d8a94bb595ab8941ea17e1ff91d
MARLAN <cpenarsky@shaw.ca>
2022-07-20 21:13:01 -0500

    change to check country

---
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/StationInfo.java
@@ -0,0 +1,19 @@
+package com.marlan.weatherupdate.utilities;
+
+import lombok.experimental.UtilityClass;
+
+import static java.lang.System.out;
+
+@UtilityClass
+public class StationInfo {
+    public static String getZoneId(String country) {
+        if (country.equals("US")) return "America/Los_Angeles"; // Nellis AFB
+        if (country.equals("AE")) return "Asia/Dubai"; // Al Dhafra AFB
+        if (country.equals("GU")) return "Pacific/Guam"; // Andersen AFB
+        if (country.equals("GE")) return "Asia/Tbilisi"; // Batumi AFB
+        if (country.equals("TR")) return "Asia/Damascus"; // Incirlik AFB
+        if (country.equals("FK")) return "Atlantic/Stanley"; // Mount Pleasant AFB
+        out.println("WARNING: Zone unknown, time conversion set to UTC");
+        return "UTC";
+    }
+}
