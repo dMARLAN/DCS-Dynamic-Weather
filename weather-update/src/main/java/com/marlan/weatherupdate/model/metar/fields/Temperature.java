0dbcabba07406c16171c5eff2b4203da45438000
MARLAN <cpenarsky@shaw.ca>
2022-07-22 19:36:28 -0500

    add temperature value as optional

--- weather-update/src/main/java/com/marlan/weatherupdate/model/metar/fields/Temperature.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/model/metar/fields/Temperature.java
@@ -2,7 +2,13 @@ package com.marlan.weatherupdate.model.metar.fields;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 public class Temperature {
     private Double value;
+
+    public Optional<Double> getValue() {
+        return Optional.ofNullable(this.value);
+    }
 }
