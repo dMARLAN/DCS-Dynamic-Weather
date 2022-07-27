96a245baa5b4e3d3bfd27e9bc90cfd712391a08a
MARLAN <cpenarsky@shaw.ca>
2022-07-27 15:51:58 -0500

    add fields

--- weather-update/src/main/java/com/marlan/weatherupdate/model/datafile/DAO.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/model/datafile/DAO.java
@@ -5,9 +5,11 @@ import lombok.Data;
 @Data
 public class DAO {
     private String avwxApiKey;
-    private String icao;
-    private String mission;
     private String discordApiKey;
+    private String spreadsheetId;
+    private String spreadsheetRange;
+    private String mission;
+    private String icao;
     private String metar;
     private String stationLongitude;
     private String stationLatitude;
