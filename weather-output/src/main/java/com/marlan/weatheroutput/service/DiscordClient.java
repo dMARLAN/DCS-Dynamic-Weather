bdab52e1ea8a7bdb93d66ed1328b72d210396a06
MARLAN <cpenarsky@shaw.ca>
2022-07-27 14:41:43 -0500

    renamed dao

--- weather-output/src/main/java/com/marlan/weatheroutput/service/DiscordClient.java
+++ weather-output/src/main/java/com/marlan/weatheroutput/service/DiscordClient.java
@@ -1,7 +1,6 @@
 package com.marlan.weatheroutput.service;
 
-import com.marlan.weatheroutput.model.WeatherOutputData;
-import lombok.NoArgsConstructor;
+import com.marlan.weatheroutput.model.DAO;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,17 +9,20 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-@NoArgsConstructor
 public class DiscordClient {
-    HttpClient httpClient = HttpClient.newHttpClient();
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
 
-    public HttpResponse<String> postChannel(WeatherOutputData weatherOutputData, String message) throws URISyntaxException, IOException, InterruptedException {
+    private DiscordClient() {
+    }
+
+    public static HttpResponse<String> postChannel(DAO dao, String message) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest postRequest = HttpRequest.newBuilder()
-                .uri(new URI(weatherOutputData.getDiscordApiKey()))
+                .uri(new URI(dao.getDiscordApiKey()))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(message))
                 .build();
 
         return httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
     }
+
 }
