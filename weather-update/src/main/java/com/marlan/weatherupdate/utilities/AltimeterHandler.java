7956d8045628f9c1a7a39bb09b16aeab79684d34
MARLAN <cpenarsky@shaw.ca>
2022-07-27 00:25:19 -0500

    refactor constants

--- weather-update/src/main/java/com/marlan/weatherupdate/utilities/AltimeterHandler.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/AltimeterHandler.java
@@ -3,6 +3,9 @@ package com.marlan.weatherupdate.utilities;
 import com.marlan.weatherupdate.model.station.AVWXStation;
 
 public class AltimeterHandler {
+    private static final double FEET_TO_METERS = 0.3048;
+    private static final double PA_TO_INHG = 0.000295299830714;
+    private static final double ISA_PRESSURE_MB = 1013.25;
 
     private AltimeterHandler() {}
 
@@ -10,18 +13,21 @@ public class AltimeterHandler {
         double stationElevFeet = station.getElevationFt();
         double stationLatitude = station.getLatitude();
         double pressureAltitude = getPressureAltitude(stationQnhInHg) + stationElevFeet;
+        System.out.println("INFO: Pressure Altitude: " + pressureAltitude + " ft");
         double stationQfeInHg = getQfe(pressureAltitude);
-        return getQff(stationQfeInHg, stationTempC, stationLatitude, stationElevFeet + 33);
+        System.out.println("INFO: Station QFE: " + stationQfeInHg + " inHg");
+        return getQff(stationQfeInHg, stationTempC, stationLatitude, stationElevFeet + 50);
     }
 
     private static double getPressureAltitude(Double stationQnhInHg) {
-        double stationQnhMb = stationQnhInHg * Constants.INHG_TO_MB;
-        return 145366.45 * (1 - Math.pow((stationQnhMb / Constants.ISA_PRESSURE_MB), 0.190284));
+        final double INHG_TO_MB = 33.864;
+        double stationQnhMb = stationQnhInHg * INHG_TO_MB;
+        return 145366.45 * (1 - Math.pow((stationQnhMb / ISA_PRESSURE_MB), 0.190284));
     }
 
     private static double getQfe(double pressureAltitude) {
         // https://en.wikipedia.org/wiki/Atmospheric_pressure
-        double h = pressureAltitude * Constants.FEET_TO_METERS;
+        double h = pressureAltitude * FEET_TO_METERS;
         double p0 = 101325; // Sea Level Standard Atmospheric Pressure (Pa)
         double t0 = 288.16; // Sea Level Standard Temperature (K)
         double g = 9.80665; // Acceleration of Gravity (m/s^2)
@@ -29,12 +35,12 @@ public class AltimeterHandler {
         double r0 = 8.314462618; // Universal Gas Constant (J/mol/K)
         double cp = 1004.68506; // Specific Heat Capacity of Air (J/kg/K)
 
-        return p0 * Math.pow(1-((g*h)/(cp*t0)),(cp*m)/(r0)) * Constants.PA_TO_INHG;
+        return p0 * Math.pow(1-((g*h)/(cp*t0)),(cp*m)/(r0)) * PA_TO_INHG;
     }
 
     private static double getQff(double qfeInHg, double temperatureInCelsius, double stationLatitude, double stationElevFeet) {
         // https://www.metpod.co.uk/calculators/pressure/ -- Swedish Meteorological and Hydrological Institute Method
-        double h = stationElevFeet * Constants.FEET_TO_METERS;
+        double h = stationElevFeet * FEET_TO_METERS;
         double t1 = getWinterInversionT1(temperatureInCelsius);
         return qfeInHg * Math.pow(Math.E,((h * 0.034163 * (1-(0.0026373 * Math.cos(stationLatitude)))) / t1));
     }
