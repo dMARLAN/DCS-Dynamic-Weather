b033b0d7fc84c752c91451b606e5aa7d1686ec6a
MARLAN <cpenarsky@shaw.ca>
2022-07-27 14:42:42 -0500

    renamed

---
+++ weather-update/src/main/java/com/marlan/weatherupdate/model/datafile/DAO.java
@@ -0,0 +1,15 @@
+package com.marlan.weatherupdate.model.datafile;
+
+import lombok.Data;
+
+@Data
+public class DAO {
+    private String avwxApiKey;
+    private String icao;
+    private String mission;
+    private String discordApiKey;
+    private String metar;
+    private String stationLongitude;
+    private String stationLatitude;
+    private String weatherType;
+}
