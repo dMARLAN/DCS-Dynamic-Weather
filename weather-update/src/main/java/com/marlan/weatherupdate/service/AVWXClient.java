dd5fe3df296bd874814b679895f8a42a332c7bbd
MARLAN <cpenarsky@shaw.ca>
2022-07-27 14:42:52 -0500

    renamed

--- weather-update/src/main/java/com/marlan/weatherupdate/service/AVWXClient.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/service/AVWXClient.java
@@ -1,6 +1,6 @@
 package com.marlan.weatherupdate.service;
 
-import com.marlan.weatherupdate.model.datafile.WeatherUpdateData;
+import com.marlan.weatherupdate.model.datafile.DAO;
 import com.marlan.weatherupdate.model.metar.AVWXMetar;
 import lombok.NoArgsConstructor;
 
@@ -15,19 +15,19 @@ import java.net.http.HttpResponse;
 public class AVWXClient {
     HttpClient httpClient = HttpClient.newHttpClient();
 
-    public HttpResponse<String> getMetar(WeatherUpdateData weatherUpdateData) throws URISyntaxException, IOException, InterruptedException {
+    public HttpResponse<String> getMetar(DAO DAO) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://avwx.rest/api/metar/" + weatherUpdateData.getStationLatitude() + "," + weatherUpdateData.getStationLongitude() + "?onfail=nearest"))
-                .header("Authorization", weatherUpdateData.getAvwxApiKey())
+                .uri(new URI("https://avwx.rest/api/metar/" + DAO.getStationLatitude() + "," + DAO.getStationLongitude() + "?onfail=nearest"))
+                .header("Authorization", DAO.getAvwxApiKey())
                 .build();
 
         return this.httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
     }
 
-    public HttpResponse<String> getStation(WeatherUpdateData weatherUpdateData, AVWXMetar weatherAVWX) throws URISyntaxException, IOException, InterruptedException {
+    public HttpResponse<String> getStation(DAO DAO, AVWXMetar weatherAVWX) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest getRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://avwx.rest/api/station/" + weatherAVWX.getStation()))
-                .header("Authorization", weatherUpdateData.getAvwxApiKey())
+                .header("Authorization", DAO.getAvwxApiKey())
                 .build();
 
         return this.httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
