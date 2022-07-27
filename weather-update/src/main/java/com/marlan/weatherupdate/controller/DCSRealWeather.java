7ed0a339fe7902b46a593adef654706e9a92e36f
MARLAN <cpenarsky@shaw.ca>
2022-07-27 14:42:36 -0500

    renamed utilities

--- weather-update/src/main/java/com/marlan/weatherupdate/controller/DCSRealWeather.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/controller/DCSRealWeather.java
@@ -3,10 +3,14 @@ package com.marlan.weatherupdate.controller;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.marlan.weatherupdate.model.datafile.WeatherUpdateData;
+import com.marlan.weatherupdate.model.datafile.DAO;
 import com.marlan.weatherupdate.model.metar.AVWXMetar;
 import com.marlan.weatherupdate.model.station.AVWXStation;
-import com.marlan.weatherupdate.service.*;
+import com.marlan.weatherupdate.service.AVWXClient;
+import com.marlan.weatherupdate.service.MissionHandlerService;
+import com.marlan.weatherupdate.utilities.DirUtility;
+import com.marlan.weatherupdate.utilities.FileUtility;
+import com.marlan.weatherupdate.utilities.MizUtility;
 
 import static java.lang.System.out;
 
@@ -14,38 +18,36 @@ public class DCSRealWeather {
 
     public static void main(String[] args) throws Exception {
         final Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-        final String dir = DirHandler.getWorkingDir(args);
+        final String dir = DirUtility.getWorkingDir(args);
         final String MISSION_FILE = "mission";
         final String DATA_FILE = "dao.json";
 
         AVWXClient avwxClient = new AVWXClient();
-        MizHandler mizHandler = new MizHandler();
-        FileHandler fileHandler = new FileHandler();
 
-        String dataContent = fileHandler.readFile(dir, DATA_FILE);
-        WeatherUpdateData weatherUpdateData = gson.fromJson(dataContent, WeatherUpdateData.class);
+        String dataContent = FileUtility.readFile(dir, DATA_FILE);
+        DAO dao = gson.fromJson(dataContent, DAO.class);
 
-        AVWXMetar metarAVWX = gson.fromJson(avwxClient.getMetar(weatherUpdateData).body(), AVWXMetar.class);
-        AVWXStation stationAVWX = gson.fromJson(avwxClient.getStation(weatherUpdateData, metarAVWX).body(), AVWXStation.class);
+        AVWXMetar metarAVWX = gson.fromJson(avwxClient.getMetar(dao).body(), AVWXMetar.class);
+        AVWXStation stationAVWX = gson.fromJson(avwxClient.getStation(dao, metarAVWX).body(), AVWXStation.class);
 
-        weatherUpdateData.setIcao(stationAVWX.getIcao());
-        fileHandler.writeJSON(dir, DATA_FILE, weatherUpdateData);
+        dao.setIcao(stationAVWX.getIcao());
+        FileUtility.writeJSON(dir, DATA_FILE, dao);
 
         if (metarAVWX.getMeta().getWarning() != null) {
             out.println("WARNING: " + metarAVWX.getMeta().getWarning());
         }
         out.println("INFO: METAR: " + metarAVWX.getSanitized());
 
-        mizHandler.extractMission(dir, weatherUpdateData.getMission());
-        String missionContent = fileHandler.readFile(dir, MISSION_FILE);
+        MizUtility.extractMission(dir, dao.getMission());
+        String missionContent = FileUtility.readFile(dir, MISSION_FILE);
 
-        MissionHandler missionHandler = new MissionHandler(weatherUpdateData.getWeatherType(), stationAVWX, metarAVWX);
-        String replacedMissionContent = missionHandler.editMission(missionContent);
+        MissionHandlerService missionHandlerService = new MissionHandlerService(dao.getWeatherType(), stationAVWX, metarAVWX);
+        String replacedMissionContent = missionHandlerService.editMission(missionContent);
 
-        fileHandler.overwriteFile(dir, MISSION_FILE, replacedMissionContent);
+        FileUtility.overwriteFile(dir, MISSION_FILE, replacedMissionContent);
 
-        mizHandler.updateMiz(dir, weatherUpdateData.getMission(), MISSION_FILE);
+        MizUtility.updateMiz(dir, dao.getMission(), MISSION_FILE);
 
-        fileHandler.deleteFile(dir, MISSION_FILE);
+        FileUtility.deleteFile(dir, MISSION_FILE);
     }
 }
