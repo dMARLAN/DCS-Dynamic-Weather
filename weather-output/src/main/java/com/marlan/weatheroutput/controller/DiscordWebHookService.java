de58925b716560239c210f54069c37d07d885572
MARLAN <cpenarsky@shaw.ca>
2022-07-27 14:41:04 -0500

    refactor

--- weather-output/src/main/java/com/marlan/weatheroutput/controller/DiscordWebHookService.java
+++ weather-output/src/main/java/com/marlan/weatheroutput/controller/DiscordWebHookService.java
@@ -3,11 +3,11 @@ package com.marlan.weatheroutput.controller;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.marlan.weatheroutput.model.WeatherOutputData;
-import com.marlan.weatheroutput.service.DirHandler;
+import com.marlan.weatheroutput.model.DAO;
 import com.marlan.weatheroutput.service.DiscordClient;
-import com.marlan.weatheroutput.service.FileHandler;
-import com.marlan.weatheroutput.service.SheetsUpdateValues;
+import com.marlan.weatheroutput.service.SheetsClient;
+import com.marlan.weatheroutput.utilities.DirHandler;
+import com.marlan.weatheroutput.utilities.FileHandler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -18,16 +18,11 @@ import static java.lang.System.out;
 public class DiscordWebHookService {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, GeneralSecurityException {
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-        DirHandler dirHandler = new DirHandler();
-        FileHandler fileHandler = new FileHandler();
-        DiscordClient discordClient = new DiscordClient();
-        final String spreadsheetId = "1BV7CuDKakK00TYXN_fIH1Vo6HqQUtWkEjMLPYy-CgRw";
-        final String spreadsheetRange = "DATA!C56";
+        final String DATA_FILE = "dao.json";
+        final String dir = DirHandler.getWorkingDir(args);
 
-        String dataFileContent = fileHandler.readFile(dirHandler.getWorkingDir(args), "dao.json");
-
-        WeatherOutputData weatherOutputData = gson.fromJson(dataFileContent, WeatherOutputData.class);
-        final String metar = weatherOutputData.getMetar();
+        String dataFileContent = FileHandler.readFile(dir, DATA_FILE);
+        DAO dao = gson.fromJson(dataFileContent, DAO.class);
 
         String jsonInput = """
                 {
@@ -38,10 +33,11 @@ public class DiscordWebHookService {
                    }
                   ]
                 }
-                """.replace("$METAR", metar);
+                """.replace("$METAR", dao.getMetar());
 
-        SheetsUpdateValues.setSheetValue(spreadsheetId, spreadsheetRange, metar);
+        SheetsClient.setSheetValue(dao.getSpreadsheetId(), dao.getSpreadsheetRange(), dao.getMetar(), dir);
 
-        out.println("Response Code: " + discordClient.postChannel(weatherOutputData, jsonInput).statusCode());
+        out.println("Response Code: " + DiscordClient.postChannel(dao, jsonInput).statusCode());
     }
+
 }
