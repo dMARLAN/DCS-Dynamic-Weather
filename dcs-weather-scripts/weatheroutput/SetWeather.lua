5326d187673422ec3adfc15bed7165d905d0cb4b
MARLAN <cpenarsky@shaw.ca>
2022-08-23 16:26:37 -0500

    change name DAO to DTO

--- dcs-weather-scripts/weatheroutput/SetWeather.lua
+++ dcs-weather-scripts/weatheroutput/SetWeather.lua
@@ -2,14 +2,14 @@ local setWeather, createMenus
 local THIS_FILE = DCSWeather.MODULE_NAME .. ".SetWeather"
 
 function setWeather(weatherType)
-    DCSWeather.JSON.setValue("weather_type", weatherType, DCSWeather.DAO)
+    DCSWeather.JSON.setValue("weather_type", weatherType, DCSWeather.DTO)
 
     local nextMissionToLoad = DCSWeather.Mission.getNextMissionName()
     if (nextMissionToLoad ~= 0) then
         trigger.action.outText("[DCSWeather.SetWeather]: Loading: " .. weatherType .. "\\" .. nextMissionToLoad .. "...", 10)
-        DCSWeather.JSON.setValue("mission", nextMissionToLoad .. ".miz", DCSWeather.DAO)
+        DCSWeather.JSON.setValue("mission", nextMissionToLoad .. ".miz", DCSWeather.DTO)
         DCSWeather.JAR.execute("weather-update")
-        DCSWeather.JSON.setValue("weather_type", "real", DCSWeather.DAO)
+        DCSWeather.JSON.setValue("weather_type", "real", DCSWeather.DTO)
         DCSWeather.Mission.loadNextMission(nextMissionToLoad)
     end
 end
