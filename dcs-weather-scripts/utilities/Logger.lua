eabb321cf3754d5bd6712e12e8ee22a0d5cc2eea
MARLAN <cpenarsky@shaw.ca>
2022-08-26 06:35:37 -0500

    change project name

--- dcs-weather-scripts/utilities/Logger.lua
+++ dcs-weather-scripts/utilities/Logger.lua
@@ -1,24 +1,24 @@
-DCSWeather.Logger = {}
+DCSDynamicWeather.Logger = {}
 
 local printLog, checkReplaceNil
 
 -- @param fileName string
 -- @param message string
-function DCSWeather.Logger.Info(logSource, message)
+function DCSDynamicWeather.Logger.info(logSource, message)
     message = checkReplaceNil(message)
     printLog(logSource, message, "INFO    ")
 end
 
 -- @param fileName string
 -- @param message string
-function DCSWeather.Logger.Warning(logSource, message)
+function DCSDynamicWeather.Logger.warning(logSource, message)
     message = checkReplaceNil(message)
     printLog(logSource, message, "WARNING ")
 end
 
 -- @param fileName string
 -- @param message string
-function DCSWeather.Logger.Error(logSource, message)
+function DCSDynamicWeather.Logger.error(logSource, message)
     message = checkReplaceNil(message)
     printLog(logSource, message, "ERROR   ")
 end
@@ -33,7 +33,7 @@ end
 
 function printLog(logSource, message, level)
     local time = os.date("%Y-%m-%d %H:%M:%S ")
-    local logFile = io.open(DCSWeather.SCRIPTS_PATH .. "\\logs\\" .. DCSWeather.MODULE_NAME .. ".log", "a")
+    local logFile = io.open(DCSDynamicWeather.SCRIPTS_PATH .. "\\logs\\" .. DCSDynamicWeather.MODULE_NAME .. ".log", "a")
     io.write(logFile, time .. level .. "[" .. logSource .. "]: " .. message .. "\n")
     io.flush(logFile)
     io.close(logFile)
