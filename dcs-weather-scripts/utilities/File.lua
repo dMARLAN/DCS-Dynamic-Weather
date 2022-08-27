1b41cee2b5331e7923641e6e54ee5cd79f45cfc0
MARLAN <cpenarsky@shaw.ca>
2022-08-27 03:30:47 -0500

    fix check

--- dcs-weather-scripts/utilities/File.lua
+++ dcs-weather-scripts/utilities/File.lua
@@ -2,7 +2,7 @@ DCSDynamicWeather.File = {}
 
 function DCSDynamicWeather.File.exists(file)
     local f = io.open(file, "rb")
-    if f then
+    if f ~= nil then
         io.close(f)
         return true
     else
