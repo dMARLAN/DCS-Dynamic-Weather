fd57b9d50d1b49ef4ab477c334fbb97303f18bca
MARLAN <cpenarsky@shaw.ca>
2022-08-27 03:30:54 -0500

    change project name

--- dcs-weather-scripts/utilities/JAR.lua
+++ dcs-weather-scripts/utilities/JAR.lua
@@ -1,11 +1,18 @@
-DCSWeather.JAR = {}
+DCSDynamicWeather.JAR = {}
 
-local THIS_FILE = DCSWeather.MODULE_NAME .. ".JAR"
+local THIS_FILE = DCSDynamicWeather.MODULE_NAME .. ".JAR"
 
-function DCSWeather.JAR.execute(jarName)
+function DCSDynamicWeather.JAR.execute(jarName)
     local THIS_METHOD = THIS_FILE .. ".executeJar"
     local jar = jarName .. ".jar"
-    DCSWeather.Logger.Info(THIS_METHOD, "Executing JAR: " .. jar)
-    os.execute("java -jar \"" .. DCSWeather.SCRIPTS_PATH .. "\\" .. jar .. "\" \"" .. DCSWeather.SCRIPTS_PATH .. "\"")
-    DCSWeather.Logger.Info(THIS_METHOD, "Execution Complete.")
+    local jarPath = DCSDynamicWeather.SCRIPTS_PATH .. "\\" .. jar
+
+    if not DCSDynamicWeather.File.exists(jarPath) then
+        DCSDynamicWeather.Logger.warning(jarName .. ".jar couldn't be read.")
+        return
+    end
+
+    DCSDynamicWeather.Logger.info(THIS_METHOD, "Executing JAR: " .. jar)
+    os.execute("java -jar \"" .. jarPath .. "\" \"" .. DCSDynamicWeather.SCRIPTS_PATH .. "\"")
+    DCSDynamicWeather.Logger.info(THIS_METHOD, "Execution Complete.")
 end
