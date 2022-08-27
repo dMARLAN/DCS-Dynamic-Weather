08c61e3dd295a72ff9fc4ccdc6857d4c314d6071
MARLAN <cpenarsky@shaw.ca>
2022-08-27 08:56:10 -0500

    convert println to logger

--- weather-output/src/main/java/com/marlan/weatheroutput/service/discord/DiscordClient.java
+++ weather-output/src/main/java/com/marlan/weatheroutput/service/discord/DiscordClient.java
@@ -1,6 +1,7 @@
 package com.marlan.weatheroutput.service.discord;
 
 import com.marlan.weatheroutput.model.DTO;
+import com.marlan.weatheroutput.utilities.Logger;
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,8 +9,6 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
-import static java.lang.System.out;
 
 public class DiscordClient {
     private static final HttpClient httpClient = HttpClient.newHttpClient();
@@ -19,14 +18,14 @@ public class DiscordClient {
 
     public static void post(DTO dto, String message) throws URISyntaxException, IOException, InterruptedException {
         if (dto.getDiscordApiKey().length() == 0) {
-            out.println("INFO: Discord API Key is empty, skipping Discord Webhook");
+            Logger.warning("Discord API key is empty");
         } else {
             HttpRequest postRequest = HttpRequest.newBuilder()
                     .uri(new URI(dto.getDiscordApiKey()))
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(message))
                     .build();
-            out.println(httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString()));
+            Logger.info("Discord API Reponse: " + httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString()).toString());
         }
     }
 
