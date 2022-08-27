a908e9a32f3ae0aa18a07e43acd94a44f35cceda
MARLAN <cpenarsky@shaw.ca>
2022-08-27 09:36:07 -0500

    add logger

---
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/Logger.java
@@ -0,0 +1,42 @@
+package com.marlan.weatherupdate.utilities;
+
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+    private static final String INFO = "INFO";
+    private static final String WARNING = "WARNING";
+    private static final String ERROR = "ERROR";
+    @Setter
+    private static String dir;
+
+    private Logger() {
+    }
+
+    @SuppressWarnings("unused")
+    public static void info(String message) {
+        log(INFO, message);
+    }
+
+    @SuppressWarnings("unused")
+    public static void warning(String message) {
+        log(WARNING, message);
+    }
+
+    @SuppressWarnings("unused")
+    public static void error(String message) {
+        log(ERROR, message);
+    }
+
+    private static void log(String type, String message) {
+        getDateTime();
+        FileHandler.appendFile(dir + "logs\\", "DiscordWebHook.log", getDateTime() + " " + type + "    " + message + "\n");
+    }
+
+    private static String getDateTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dtf.format(java.time.LocalDateTime.now());
+    }
+
+}
