da9c5c483ad831d72b7964b0d50b9d41d4bd78a1
MARLAN <cpenarsky@shaw.ca>
2022-09-23 17:52:57 -0500

    add comment

--- weather-update/src/main/java/com/marlan/weatherupdate/model/metar/fields/Time.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/model/metar/fields/Time.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class Time {
-    private String repr;
-    private String dt;
+    private String repr; // var name from AVWX API; e.g.: "232221Z"
+    private String dt; // var name from AVWX API; e.g.: "2022-09-23T22:21:00Z"
 }
