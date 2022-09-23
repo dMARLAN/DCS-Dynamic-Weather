949ec8f3b0f40fb9eb6ee265aa061db34caaeb92
MARLAN <cpenarsky@shaw.ca>
2022-09-23 15:53:07 -0500

    check directory is valid

--- weather-output/src/main/java/com/marlan/weatheroutput/utilities/DirHandler.java
+++ weather-output/src/main/java/com/marlan/weatheroutput/utilities/DirHandler.java
@@ -1,5 +1,8 @@
 package com.marlan.weatheroutput.utilities;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static java.lang.System.getProperty;
 
 public class DirHandler {
@@ -7,11 +10,15 @@ public class DirHandler {
     }
 
     public static String getWorkingDir(String[] args) {
-        if (args.length != 0) {
+        if (args.length != 0 && Files.exists(Path.of(args[0]))) {
             System.setProperty("user.dir", args[0]);
         }
         String workingDir = getProperty("user.dir") + "\\";
         Logger.setDir(workingDir);
+        if (!Files.exists(Path.of(workingDir))) {
+            Logger.error("Working Directory inaccessible.");
+            System.exit(1); // Unrecoverable Error
+        }
         Logger.info("Working Directory: " + workingDir);
         return workingDir;
     }
