1835df65fe5230cb5235bbd9628ca1c9fb0eba0c
MARLAN <cpenarsky@shaw.ca>
2022-09-29 13:47:50 -0500

    remove unnecessary exceptions

--- weather-output/src/main/java/com/marlan/weatheroutput/WeatherOutput.java
+++ weather-output/src/main/java/com/marlan/weatheroutput/WeatherOutput.java
@@ -5,8 +5,6 @@ import com.marlan.weatheroutput.utilities.DirHandler;
 import com.marlan.weatheroutput.utilities.Log;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.GeneralSecurityException;
 
 /**
  * Entry Point for the Weather Output module of DCS Dynamic Weather
@@ -14,7 +12,7 @@ import java.security.GeneralSecurityException;
  * @author Chad Penarsky
  */
 public class WeatherOutput {
-    public static void main(String[] args) throws IOException, GeneralSecurityException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         final String WORKING_DIR = DirHandler.getWorkingDir(args);
         Log.open(WORKING_DIR);
         Log.info("Working Directory: " + WORKING_DIR);
