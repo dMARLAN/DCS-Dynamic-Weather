ee09eb893f5442441aa10f8fb55db00779248655
MARLAN <cpenarsky@shaw.ca>
2022-09-29 10:47:52 -0500

    catch null exception from TimeZoneMap

--- weather-update/src/main/java/com/marlan/weatherupdate/utilities/StationInfoUtility.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/utilities/StationInfoUtility.java
@@ -2,6 +2,8 @@ package com.marlan.weatherupdate.utilities;
 
 import us.dustinj.timezonemap.TimeZoneMap;
 
+import java.util.Objects;
+
 /**
  * Returns Time Zone Id based on latitude & longitude using TimeZoneMap otherwise returns UTC if TimeZoneMap fails.
  */
@@ -11,12 +13,12 @@ public class StationInfoUtility {
 
     public static String getZoneId(double latitude, double longitude) {
         TimeZoneMap map = TimeZoneMap.forRegion(latitude - 1, longitude - 1, latitude + 1, longitude + 1);
-        String zoneId = map.getOverlappingTimeZone(latitude, longitude).getZoneId();
-        if (!zoneId.isEmpty()) {
+        try {
+            String zoneId = Objects.requireNonNull(map.getOverlappingTimeZone(latitude, longitude)).getZoneId();
             Log.info("ZoneId: " + zoneId);
             return zoneId;
-        } else {
-            Log.warning("Unable to get ZoneId, using default ZoneId: UTC");
+        } catch (NullPointerException npe) {
+            Log.warning("Unable to get ZoneId, using UTC");
             return "UTC";
         }
     }
