60ba19019ccd1f3148de429dac0d1b402a31bb49
MARLAN <cpenarsky@shaw.ca>
2022-10-06 09:06:47 -0500

    add first test

--- weather-update/src/test/java/com/marlan/weatherupdate/utilities/StationInfoUtilityTest.java
+++ weather-update/src/test/java/com/marlan/weatherupdate/utilities/StationInfoUtilityTest.java
@@ -1,5 +1,6 @@
 package com.marlan.weatherupdate.utilities;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class StationInfoUtilityTest {
 
     @Test
-    void myTestTest() {
-        assertTrue(true);
+    @DisplayName("Invalid latititude and longitude should return UTC")
+    void invalidLatLongShouldReturnUTC() {
+        assertEquals("UTC", StationInfoUtility.getZoneId(-1, -1));
     }
 
 }
