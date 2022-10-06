df687b5e71679b49b22e46f6eae63b8869bfa88b
MARLAN <cpenarsky@shaw.ca>
2022-10-06 08:33:01 -0500

    initial commit

---
+++ weather-update/src/test/java/com/marlan/weatherupdate/utilities/StationInfoUtilityTest.java
@@ -0,0 +1,14 @@
+package com.marlan.weatherupdate.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StationInfoUtilityTest {
+
+    @Test
+    void myTestTest() {
+        assertTrue(true);
+    }
+
+}
