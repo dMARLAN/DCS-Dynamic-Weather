cf107a536f309e3ef497bfbbf60c4c26b49976a6
MARLAN <cpenarsky@shaw.ca>
2022-10-06 09:33:01 -0500

    add tests

--- weather-update/src/test/java/com/marlan/weatherupdate/utilities/StationInfoUtilityTest.java
+++ weather-update/src/test/java/com/marlan/weatherupdate/utilities/StationInfoUtilityTest.java
@@ -1,16 +1,35 @@
 package com.marlan.weatherupdate.utilities;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StationInfoUtilityTest {
+    static Log log = Log.getInstance();
+
+    @BeforeAll
+    static void setup() {
+        log.disable();
+    }
 
     @Test
-    @DisplayName("Invalid latititude and longitude should return UTC")
+    @DisplayName("Invalid latitude and longitude should return UTC")
     void invalidLatLongShouldReturnUTC() {
-        assertEquals("UTC", StationInfoUtility.getZoneId(-1, -1));
+        assertEquals("UTC", StationInfoUtility.getZoneId(-9999, -9999));
+    }
+
+    @Test
+    @DisplayName("Valid latitude and longitude should return ZoneId")
+    void validLatLongShouldReturnZoneId() {
+        assertAll(
+                () -> assertEquals("America/New_York", StationInfoUtility.getZoneId(40.7128, -74.0060)),
+                () -> assertEquals("America/Los_Angeles", StationInfoUtility.getZoneId(34.0522, -118.2437)),
+                () -> assertEquals("America/Chicago", StationInfoUtility.getZoneId(41.8781, -87.6298)),
+                () -> assertEquals("America/Denver", StationInfoUtility.getZoneId(39.7392, -104.9903))
+        );
     }
 
 }
