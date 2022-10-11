17d56d4839d7d7cc325e524cef4fba540a3a028e
MARLAN <cpenarsky@shaw.ca>
2022-10-10 21:24:49 -0500

    exit program if directory is inaccessible, remove IOException

--- shared/src/main/java/com/marlan/shared/utilities/DirHandler.java
+++ shared/src/main/java/com/marlan/shared/utilities/DirHandler.java
@@ -2,7 +2,6 @@ package com.marlan.shared.utilities;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -11,22 +10,26 @@ import static java.lang.System.getProperty;
 /**
  * Returns working directory using either entry points's main String[] args passed through
  * from weather-scripts/utilities/JAR or if args are empty, then use user.dir
+ * If args directory does not exist or cannot be accessed, program exits because if a custom directory
+ * is specified, then it is required to be accessible.
  */
 public class DirHandler {
     private DirHandler() {
     }
 
     @NotNull
-    public static String getWorkingDir(String[] args) throws IOException {
+    public static String getWorkingDir(String[] args) {
         if (args != null && args.length != 0) {
             if (!Files.exists(Path.of(args[0]))) {
-                throw new IOException("Directory does not exist: " + args[0]);
+                System.out.println("ERROR: Directory does not exist: " + args[0]);
+                System.exit(1); // Unrecoverable error
             }
             System.setProperty("user.dir", args[0]);
         }
         String workingDir = getProperty("user.dir") + "\\";
         if (!Files.exists(Path.of(workingDir))) {
-            throw new IOException("Working directory inaccessible");
+            System.out.println("ERROR: Directory inaccessible " + workingDir);
+            System.exit(1); // Unrecoverable error
         }
         return workingDir;
     }
