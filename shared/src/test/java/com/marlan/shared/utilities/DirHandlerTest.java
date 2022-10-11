ea1149140d3cd383e87484323a74543a80159561
MARLAN <cpenarsky@shaw.ca>
2022-10-10 21:17:32 -0500

    initial tests commit

---
+++ shared/src/test/java/com/marlan/shared/utilities/DirHandlerTest.java
@@ -0,0 +1,78 @@
+package com.marlan.shared.utilities;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class DirHandlerTest {
+
+    @Test
+    @DisplayName("Empty Args should return user.dir")
+    @Order(1)
+    void EmptyArgsShouldEqualSharedDir() {
+        String[] args = {};
+        String workingDir = "";
+        try {
+            workingDir = DirHandler.getWorkingDir(args);
+        } catch (IOException ioe) {
+            fail("Exception thrown: " + ioe.getMessage());
+        }
+        assertTrue(workingDir.endsWith("shared\\"));
+    }
+
+    @Test
+    @DisplayName("Null Args should return user.dir")
+    @Order(2)
+    void NullArgsShouldEqualSharedDir() {
+        String workingDir = "";
+        try {
+            workingDir = DirHandler.getWorkingDir(null);
+        } catch (IOException ioe) {
+            fail("Exception thrown: " + ioe.getMessage());
+        }
+        assertTrue(workingDir.endsWith("shared\\"));
+    }
+
+    @Test
+    @DisplayName("Nonexistent Args Directory should throw IOException")
+    @Order(2)
+    void NonexistentArgsDirShouldThrowIOException() {
+        assertThrows(IOException.class, () -> DirHandler.getWorkingDir(new String[]{"nonexistentDirectory"}));
+    }
+
+    @Test
+    @DisplayName("Valid Args should return args[0]")
+    void ValidArgsShouldEqualArgsDir() {
+        // Setup
+        String testDirName = "testDir";
+        Path testDir = Path.of(testDirName);
+        try {
+            Files.createDirectories(testDir);
+        } catch (IOException ioe) {
+            fail("Exception thrown: " + ioe.getMessage());
+        }
+
+        // Test Block
+        String[] args = {testDirName};
+        String workingDir = "";
+        try {
+            workingDir = DirHandler.getWorkingDir(args);
+        } catch (IOException ioe) {
+            fail("Exception thrown: " + ioe.getMessage());
+        }
+        assertTrue(workingDir.endsWith("testDir\\"));
+
+        // Teardown
+        try {
+            Files.delete(testDir);
+        } catch (IOException ioe) {
+            fail("Exception thrown: " + ioe.getMessage());
+        }
+    }
+
+}
