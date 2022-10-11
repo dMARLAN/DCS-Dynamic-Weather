63ec4f23ac1d2a6e735a4c960bffa7d342d58df9
MARLAN <cpenarsky@shaw.ca>
2022-10-10 20:35:33 -0500

    add tests

---
+++ shared/src/test/java/com/marlan/shared/utilities/FileHandlerTest.java
@@ -0,0 +1,77 @@
+package com.marlan.shared.utilities;
+
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FileHandlerTest {
+
+    @BeforeAll
+    static void setup() {
+        File file = new File("test.txt");
+        try (FileWriter fileWriter = new FileWriter(file, false)) {
+            fileWriter.write("TestContents");
+        } catch (IOException ioe) {
+            fail("IOException was thrown creating test file.");
+        }
+    }
+
+    @AfterAll
+    static void teardown() {
+        Path filePath = Path.of("test.txt");
+        try {
+            if (Files.exists(filePath)) {
+                Files.delete(filePath);
+            }
+        } catch (IOException ioe) {
+            fail("IOException was thrown deleting test file.");
+        }
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("File should match test contents")
+    void fileReadShouldEqualTestContents() {
+        String readResult = FileHandler.readFile("", "test.txt");
+        assertEquals("TestContents", readResult);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("File should be overwritten with new contents")
+    void fileOverwriteShouldEqualNewContents() {
+        FileHandler.overwriteFile("", "test.txt", "NewContents");
+        String readResult = FileHandler.readFile("", "test.txt");
+        assertEquals("NewContents", readResult);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("File should not exist when deleted")
+    void fileDeleteShouldNotExist() {
+        FileHandler.deleteFile("", "test.txt");
+        Path filePath = Path.of("test.txt");
+        assertFalse(Files.exists(filePath));
+    }
+
+    @Test
+    @DisplayName("Not existent file should return empty string")
+    void fileReadShouldReturnEmptyString() {
+        String readResult = FileHandler.readFile("", "nonexistent.txt");
+        assertEquals("", readResult);
+    }
+
+    @Test
+    @DisplayName("Deleting non existent file throws no exception")
+    void fileDeleteShouldNotDelete() {
+        assertDoesNotThrow(() -> FileHandler.deleteFile("", "nonexistent.txt"));
+    }
+
+}
