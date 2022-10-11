5be43e94886b89210da8ff0172709858694d5f3a
MARLAN <cpenarsky@shaw.ca>
2022-10-10 20:35:44 -0500

    import assertions.*

--- shared/src/test/java/com/marlan/shared/utilities/LogTest.java
+++ shared/src/test/java/com/marlan/shared/utilities/LogTest.java
@@ -2,12 +2,14 @@ package com.marlan.shared.utilities;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class LogTest {
     @Test
     @DisplayName("Logger should only have one instance")
     void loggerShouldBeSingleton(){
         Log logger1 = Log.getInstance();
         Log logger2 = Log.getInstance();
-        Assertions.assertEquals(logger1, logger2);
+        assertEquals(logger1, logger2);
     }
 }
