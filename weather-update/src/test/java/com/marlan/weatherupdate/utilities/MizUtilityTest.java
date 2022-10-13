5a30cc38a81ffeeb98e06578475df91e84f4c518
MARLAN <cpenarsky@shaw.ca>
2022-10-13 16:15:53 -0500

    remove unnecessary reflection

--- weather-update/src/test/java/com/marlan/weatherupdate/utilities/MizUtilityTest.java
+++ weather-update/src/test/java/com/marlan/weatherupdate/utilities/MizUtilityTest.java
@@ -4,55 +4,25 @@ import com.marlan.shared.model.Config;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MizUtilityTest {
-    private static final String sevenZipPathVarName = "sevenZipPath";
+    private static final Config config = new Config();
 
     @Test
     @DisplayName("Empty custom config path should return default 7zip path")
     void emptyCustomConfigPathShouldReturnDefaultSevenzipPath() {
-        Config config = new Config();
         config.setCustomSevenZipPath("");
-        MizUtility mizUtility = new MizUtility(config);
         String expectedPath = System.getenv("ProgramFiles") + "\\7-Zip\\7z.exe";
-        String actual = null;
-
-        try {
-            Field sevenZipPathField = mizUtility.getClass().getDeclaredField(sevenZipPathVarName);
-            sevenZipPathField.setAccessible(true);
-            actual = sevenZipPathField.get(mizUtility).toString();
-        } catch (NoSuchFieldException nsfe){
-            fail(sevenZipPathVarName + " field not found");
-        } catch (IllegalAccessException iae){
-            fail(sevenZipPathVarName + " field not accessible");
-        }
-
-        assertEquals(expectedPath, actual);
+        assertEquals(expectedPath, new MizUtility(config).getSevenZipPath());
     }
 
     @Test
     @DisplayName("Custom config path should return custom 7zip path")
     void customConfigPathShouldReturnCustomSevenzipPath() {
-        Config config = new Config();
         String expectedPath = "C:\\Test1\\Test2\\7z.exe";
         config.setCustomSevenZipPath(expectedPath);
-        MizUtility mizUtility = new MizUtility(config);
-        String actualPath = null;
-
-        try {
-            Field sevenZipPathField = mizUtility.getClass().getDeclaredField(sevenZipPathVarName);
-            sevenZipPathField.setAccessible(true);
-            actualPath = sevenZipPathField.get(mizUtility).toString();
-        } catch (NoSuchFieldException nsfe){
-            fail(sevenZipPathVarName + " field not found");
-        } catch (IllegalAccessException iae){
-            fail(sevenZipPathVarName + " field not accessible");
-        }
-
-        assertEquals(expectedPath, actualPath);
+        assertEquals(expectedPath, new MizUtility(config).getSevenZipPath());
     }
 
 }
