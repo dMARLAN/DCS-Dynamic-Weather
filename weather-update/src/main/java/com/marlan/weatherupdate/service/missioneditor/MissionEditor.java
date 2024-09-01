7ed11e16074b9a9db5827f90ca46616fd5d7a3ac
Oskar Rasienski <oskar.rasienski@gmail.com>
2024-09-01 03:19:29 +0200

    Fixed replaceHour regex bug

--- weather-update/src/main/java/com/marlan/weatherupdate/service/missioneditor/MissionEditor.java
+++ weather-update/src/main/java/com/marlan/weatherupdate/service/missioneditor/MissionEditor.java
@@ -133,7 +133,7 @@ public class MissionEditor {
 
     @NotNull
     private String replaceHour(String mission, float hour) {
-        Pattern pattern = Pattern.compile("(?:\\s{4}|\\t)\\[\"start_time\"]\\s=\\s.*,", Pattern.MULTILINE);
+        Pattern pattern = Pattern.compile("^(?:\\s{4}|\\t)\\[\"start_time\"]\\s=\\s.*,$", Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(mission);
         if (!matcher.find()) {
             log.error("Regex match failed, Hour not set.");
